b896276874d7c0ceb4948cdfc0b2045939619826
Vladislav <gavrilin@phystech.edu>
2016-11-01 14:07:48 +0300

    Complete App

---
+++ ButtonTextMVVM/ButtonTextVM.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Threading.Tasks;
+using Agilent.AgilentN57xx.Interop;
+namespace ButtonTextMVVM
+{
+    class ButtonTextVM : INotifyPropertyChanged
+    {
+        private string currentPart = null;
+        private string voltagePart = null;
+        AgilentN57xx driver = new AgilentN57xx();
+        public ButtonTextVM()
+          {
+
+          }
+        public string ConnectionStatus()
+        {
+           
+                if (driver.Initialized == true)
+                {
+                    return Convert.ToString(driver.Identity.InstrumentModel)+" подключен";
+                  
+            }
+                else
+        {
+                   return "Источник питания отключен";
+                   
+        }
+
+        }
+            
+        
+        public AgilentN57xx Connect(string ModelType)
+        {
+
+           string resourceDesc = "GPIB0::5::INSTR";
+           if (ModelType == "N5768A")
+           {
+               resourceDesc = "TCPIP0::169.254.84.86::INSTR";
+               string initOptions = "QueryInstrStatus=true, Simulate=false, DriverSetup= Model=N5768A, Trace=false, TraceName=c:\\temp\\traceOut";
+               try
+               {
+                   bool idquery = true;
+                   bool reset = true;
+                   driver.Initialize(resourceDesc, idquery, reset, initOptions);
+
+
+
+
+               }
+               catch (Exception e)
+               {
+
+                   MessageBox.Show(e.Message);
+
+
+               }
+           }
+           if (ModelType == "N5745A")
+           {
+               resourceDesc = "TCPIP0::169.254.84.86::INSTR";
+               string initOptions = "QueryInstrStatus=true, Simulate=true, DriverSetup= Model=N5745A , Trace=false, TraceName=c:\\temp\\traceOut";
+               try
+               {
+                   bool idquery = true;
+                   bool reset = true;
+                   driver.Initialize(resourceDesc, idquery, reset, initOptions);
+
+
+
+
+               }
+               catch (Exception e)
+               {
+
+                   MessageBox.Show(e.Message);
+
+
+               }
+           }
+
+           
+           
+          
+           return driver;      
+       }
+        public void  Disconnect()
+        {
+            if (driver.Initialized == true)
+                driver.Close();
+            else
+                MessageBox.Show("Driver is disconnected");
+        }
+        public string CurrentLimitPart
+        {
+           set
+            {
+                if (value == currentPart) return;
+                
+                OnPropertyChanged();
+                
+                 try 
+                 {
+                     var currentLimit = Convert.ToDouble(value);
+                     driver.Output.CurrentLimit = currentLimit;
+                     OnPropertyChanged();
+                     
+               
+                 }
+                catch (Exception e)
+                 { 
+                    MessageBox.Show(e.Message);
+                }
+                
+                
+            }
+        }
+        public string VoltagePart
+        {
+            get { return voltagePart; }
+            set
+            {
+                //if (value == voltagePart) return;
+                voltagePart = value;
+                OnPropertyChanged();
+                try
+                {
+                    var voltage = Convert.ToDouble(value);
+                    if ((voltage <= 70))
+                    {
+                        
+                        
+                        //driver.Output.Protection.CurrentLimitBehavior = AgilentN57xxCurrentLimitBehaviorEnum.AgilentN57xxCurrentLimitTrip;
+                        
+                        driver.Output.VoltageLevel = voltage;  // initial voltage setting
+                        System.Threading.Thread.Sleep(1000); // delay for output to settle
+                        
+                        
+                    }
+                    else
+                    {
+                        MessageBox.Show("Current must be lower than 2");
+                    }
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+            }
+
+        }
+        public string OVPPart
+        {
+            get { return OVPPart; }
+            set
+            {
+                try
+                {
+                    var ovp = Convert.ToDouble(value);
+                                          //driver.Output.Protection.CurrentLimitBehavior = AgilentN57xxCurrentLimitBehaviorEnum.AgilentN57xxCurrentLimitTrip;
+                        driver.Output.Protection.OVPLimit = ovp;
+                       
+                        System.Threading.Thread.Sleep(1000); // delay for output to settle
+                }
+                   
+                
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+            }
+            }
+        public string UVLPart
+        {
+            get { return UVLPart; }
+            set
+            {
+                try
+                {
+                    var uvl = Convert.ToDouble(value);
+                    //driver.Output.Protection.CurrentLimitBehavior = AgilentN57xxCurrentLimitBehaviorEnum.AgilentN57xxCurrentLimitTrip;
+                    driver.Output.MinVoltageLimit = uvl;
+
+                    System.Threading.Thread.Sleep(1000); // delay for output to settle
+                }
+
+
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+            }
+        }
+        public string OCPPart
+        {
+            set
+            {
+                if (value=="On")
+                {
+                    driver.Output.Protection.CurrentLimitBehavior = AgilentN57xxCurrentLimitBehaviorEnum.AgilentN57xxCurrentLimitTrip;
+                }
+                if (value=="Off")
+                {
+                    driver.Output.Protection.CurrentLimitBehavior = AgilentN57xxCurrentLimitBehaviorEnum.AgilentN57xxCurrentLimitRegulate;
+                }
+            }    
+        }
+        
+        public string OVPStatus()
+        {
+            if (driver.Initialized == true)
+            {
+                double s = driver.Output.Protection.OVPLimit;
+                string ovpStatus = Convert.ToString(s);
+                return ovpStatus;
+                //OnPropertyChanged();
+            }
+            else return "";
+        }
+        public string CurrentLimitStatus()
+        {
+            if (driver.Initialized == true)
+            {
+                double s = driver.Output.CurrentLimit;
+                string ovpStatus = Convert.ToString(s);
+                return ovpStatus;
+               
+            }
+            else return "";
+        }
+        public string CurrentStatus()
+        {
+            if (driver.Initialized == true)
+            {
+                double s = driver.Output.Measure(AgilentN57xxMeasureTypeEnum.AgilentN57xxMeasurementCurrent);
+                string ovpStatus = Convert.ToString(s);
+                return ovpStatus;
+                //OnPropertyChanged();
+            }
+            else return "";
+        }
+        public string UVLStatus()
+        {
+            if (driver.Initialized == true)
+            {
+                double s = driver.Output.MinVoltageLimit;
+                string uvlStatus = Convert.ToString(s);
+                return uvlStatus;
+                //OnPropertyChanged();
+            }
+            else return "";
+        }
+        public string OCPStatus()
+        {
+            if (driver.Initialized == true)
+            {
+                AgilentN57xxCurrentLimitBehaviorEnum s = driver.Output.Protection.CurrentLimitBehavior;
+                string st = Convert.ToString(s);
+                if (st=="AgilentN57xxCurrentLimitRegulate")
+                    return "Off";
+                if (st == "AgilentN57xxCurrentLimitTrip")
+                    return "On";
+                else return "Null";
+
+                //OnPropertyChanged();
+            }
+            else return "";
+        }
+
+        public string VoltageStatus()
+        {
+            if (driver.Initialized==true){
+                double s = driver.Output.Measure(AgilentN57xxMeasureTypeEnum.AgilentN57xxMeasurementVoltage);
+                string voltageStatus = Convert.ToString(s);
+                return voltageStatus;
+                //OnPropertyChanged();
+            }
+            else return "";
+        }
+
+        public void OutputEnable(bool state)
+        {
+            driver.Output.Enabled = state;
+        }
+
+        public string OutputStatus()
+        {
+            if (driver.Initialized == true)
+            {
+                if (driver.Output.Enabled == true)
+                    return "Выход включен";
+                else
+                    return "Выход отключен";
+            }
+            else return "Выход отключен";
+        }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+    }
+}
---
+++ ButtonTextMVVM/MainWindow.xaml.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ButtonTextMVVM
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ButtonTextVM btn;
+        private readonly SynchronizationContext _context;
+     
+        public MainWindow()
+        {
+            InitializeComponent();
+            btn = new ButtonTextVM();
+            this.DataContext = btn;
+            _context = SynchronizationContext.Current;
+           
+            DoLoop();
+           
+           
+        }
+        private void Send_Click(object sender, RoutedEventArgs e)
+        {
+            if ((CurrentLimit.Text == "") || (Voltage.Text == "") || (OVP.Text == "") || (UVL.Text == "") || (UVL.Text == ""))
+                MessageBox.Show("Заполните все поля");
+            else
+            {
+                if ((Convert.ToDouble(Voltage.Text) < 0) && (Convert.ToDouble(Voltage.Text) > 80))
+                    MessageBox.Show("Напряжение должно быть не меньше нуля и не больше 80В");
+                else
+                {
+                    if ((Convert.ToDouble(Voltage.Text) < 0) && (Convert.ToDouble(Voltage.Text) > 19))
+                        MessageBox.Show("Ограничение силы тока должно быть не меньше нуля и не больше 19А");
+                    else
+                    {
+                        if ((Convert.ToDouble(Voltage.Text) < 0) && (Convert.ToDouble(Voltage.Text) > 19))
+                            MessageBox.Show("Ограничение силы тока должно быть не меньше нуля и не больше 19А");
+                        else
+                        {
+                            if ((Convert.ToDouble(OVP.Text) < 0) && (Convert.ToDouble(OVP.Text) > 80) || (Convert.ToDouble(UVL.Text) < 0) && (Convert.ToDouble(UVL.Text) > 80))
+                                MessageBox.Show("OVP и UVL быть не меньше нуля и не больше 80В");
+                            else
+                            {
+                                btn.VoltagePart = Voltage.Text;
+                                btn.CurrentLimitPart = CurrentLimit.Text;
+                                btn.OVPPart = OVP.Text;
+                                btn.UVLPart = UVL.Text;
+                                btn.OCPPart = OCP.Text;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+       
+        private void Connect_Click(object sender, RoutedEventArgs e)
+        {
+           
+                btn.Connect(ModelType.Text);
+            
+        }
+        private void Disonnect_Click(object sender, RoutedEventArgs e)
+        {
+           
+                btn.Disconnect();
+            
+        }
+        private void OutputOn_CLick(object sender, RoutedEventArgs e)
+        {
+            btn.OutputEnable(true);
+        }
+        private void OutputOff_Click(object sender, RoutedEventArgs e)
+        {
+            btn.OutputEnable(false);
+        }
+        private void DoLoop()
+        {
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    Thread.Sleep(1000);
+                    //lblTimer.Text = DateTime.Now.ToLongTimeString(); // no work
+                    UpdateConnectionStatus(); // work
+                }
+            });
+        }
+      
+        private void UpdateConnectionStatus()
+        {
+            //SynchronizationContext.Current, here, is context of running thread (Task)
+            _context.Post(setConnectionStatus, btn.ConnectionStatus());
+            _context.Post(setVoltageStatus, btn.VoltageStatus());
+            _context.Post(setOutputStatus, btn.OutputStatus());
+            _context.Post(setOvpStatus, btn.OVPStatus());
+            _context.Post(setUvlStatus, btn.UVLStatus());
+            _context.Post(setCurrentStatus, btn.CurrentStatus());
+            _context.Post(setCurrentLimitStatus, btn.CurrentLimitStatus());
+            _context.Post(setOCPStatus, btn.OCPStatus());
+           
+            
+        }
+       
+        public void setConnectionStatus(object content)
+        {
+            StatusBar.Text = (string)content;
+        }
+      
+        public void setVoltageStatus(object content)
+        {
+            VoltageStatus.Text = (string)content;
+        }
+        public void setCurrentStatus(object content)
+        {
+           CurrentStatus.Text = (string)content;
+        }
+        public void setCurrentLimitStatus(object content)
+        {
+            CurrentLimitStatus.Text = (string)content;
+        }
+        public void setOutputStatus(object content)
+        {
+            OutputStatus.Text = (string)content;
+        }
+        public void setOvpStatus(object content)
+        {
+            OVPStatus.Text = (string)content;
+        }
+        public void setUvlStatus(object content)
+        {
+            UVLStatus.Text = (string)content;
+        }
+        public void setOCPStatus(object content)
+        {
+            OCPStatus.Text = (string)content;
+        }
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
